34ac8e7a6c22d11bc76cab60e62fd8aeb19ea57d
Kashyap Pandya <17811129+Kashyap-Pandya@users.noreply.github.com>
2022-11-22 19:24:00 +0530

    [ADD] intial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const form = document.getElementById("form")
+const names = document.getElementById("Bname");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const read = document.getElementById("read")
+const button = document.getElementById("submit")
+
+let myLibrary = [];
+
+
+
+
+
+// function addBookToLibrary() {
+//     form.addEventListener("input",(e) => {
+//         if(e.target === Bname && e.target === author && e.target === pages && e.target ===read){
+//             let bookName = e.target.value;
+//             obj.book= bookName;    
+//         }
+//         if(e.target === author) {
+//             let Bauthor = e.target.value
+//             obj.author = Bauthor;
+//         }
+//         if(e.target === pages){
+//             let Bpages = e.target.value;
+//             obj.pages = Bpages
+//         }
+//         if(e.target === read){
+//             let Bread = e.target.value;
+//             obj.read = Bread
+//         }
+//         myLibrary.push(obj)
+//     })
+   
+// }
+
+// addBookToLibrary()
+
+// console.log(myLibrary)
+// // console.log(obj)
